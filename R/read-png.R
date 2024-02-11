111c5d363970ee35e7177dd3edf4ba5bc3c13821
mike <mikefc@coolbutuseless.com>
2024-02-11 20:54:39 +1000

    docs [no ci]

--- R/read-png.R
+++ R/read-png.R
@@ -1,23 +1,26 @@
 #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-#' Read a PNG from a raw vector in memory
+#' Read a PNG 
 #'
-#' See \url{https://libspng.org/docs/decode/} for more information on which
-#' combinations of formats and flags are permissable.
-#'
-#' @param src raw vector containing encoded PNG data or file path
-#' @param type type of object in which to store image data. Valid types are
+#' @param src PNG image provided as either a file path, or a raw vector 
+#'        containing encoded PNG data
+#' @param type type of R object in which to store image data. Valid types are
 #'        'array', 'raster', 'native_raster', 'indexed' and 'raw'.  Note that indexed
 #'        image objects can only be loaded from indexed PNGs.
 #' @param rgba Should the result be forced into RGBA?  Default: FALSE  
-#'        Example 1: When reading with \code{type = 'native_raster'} the result
-#'        will always be RGBA, so this variable is ignored.
-#'        Example 2: When reading a grey+alpha image, if \code{rgba = TRUE}, 
-#'        then image will be read into an RGBA array with 4 planes.  Otherwise
-#'        it will be read in as grey+alpha data in an array with 2 planes.
-#' @param flags flags. default: 1 (always decode transparency from tRNS chunks). 
+#'        means to use the most appropriate format of the given R image type 
+#'        to store the data.  If \code{TRUE}, then the image will be forced
+#'        into RGBA colour mode.
+#' @param flags Flags to apply when reading PNG. Default: 1 (always decode transparency from tRNS chunks). 
 #'        See \code{?spng_decode_flags} for other options.  Must be an integer.
-#' @param avoid_transpose default: FALSE.
-#' @param array_type 'dbl' or 'int'
+#' @param avoid_transpose Default: FALSE. If \code{TRUE}, then transposing the image
+#'        from row-major (in the PNG), into column-major (in R) will be avoided
+#'        if possible.  This option only applies when reading grayscale or
+#'        indexed images.  Since the transposition is avoided, the decode 
+#'        step can be faster, but the image will not be in the correct orientation.
+#' @param array_type 'dbl' or 'int'. Default: dbl.  When reading PNG into an array,
+#'        should the data be stored as a double (i.e. real) in the range [0, 1] 
+#'        or an integer in the range [0,255] (for 8 bit images) or 
+#'        [0,65535] (for 16 bit images).
 #'
 #' @return R image object of the specified type
 #'
